be34ef2914f0d2c81c0e88eff830bcb52ac31362
Alwaysproblem <reganyang0415@gmail.com>
2025-01-22 04:08:17 +0000

    change format

--- grid_and_block/device.cu
+++ grid_and_block/device.cu
@@ -1,18 +1,19 @@
 #include <stdio.h>
+
 #include <cstdio>
+
 #include "device.h"
 
-__global__ void checkIndex_imp(void)
-{
-    printf("threadIdx:(%d, %d, %d)\n", threadIdx.x, threadIdx.y, threadIdx.z);
-    printf("blockIdx:(%d, %d, %d)\n", blockIdx.x, blockIdx.y, blockIdx.z);
-    printf("--------------------------------\n");
-    printf("blockDim:(%d, %d, %d)\n", blockDim.x, blockDim.y, blockDim.z);
-    printf("gridDim:(%d, %d, %d)\n", gridDim.x, gridDim.y, gridDim.z);
+__global__ void checkIndex_imp(void) {
+  printf(
+      "threadIdx:(%d, %d, %d) blockIdx:(%d, %d, %d) blockDim:(%d, %d, %d) "
+      "gridDim:(%d, %d, %d)\n",
+      threadIdx.x, threadIdx.y, threadIdx.z, blockIdx.x, blockIdx.y, blockIdx.z,
+      blockDim.x, blockDim.y, blockDim.z, gridDim.x, gridDim.y, gridDim.z);
 }
 
 // 在 device.cu 中封装函数调用
 extern "C" void checkIndex(dim3 grid, dim3 block) {
-    checkIndex_imp<<<grid, block>>>();
-    cudaDeviceReset();
+  checkIndex_imp<<<grid, block>>>();
+  cudaDeviceReset();
 }

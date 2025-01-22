bef150817949c7a657bf620cf2806a680312bdce
Alwaysproblem <reganyang0415@gmail.com>
2025-01-22 10:40:45 +0000

    Added result checker

--- sumArray/main.cc
+++ sumArray/main.cc
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
 #include <chrono>
 #include <iostream>
+
 #include "device.h"
 
 void printVector(float *vector, const int N) {
@@ -26,6 +28,24 @@ void initialData(float *ip, int size) {
   for (int i = 0; i < size; i++) {
     ip[i] = (float)(rand() & 0xFF) / 10.0f;
   }
+
+  return;
+}
+
+void checkResult(float *hostRef, float *gpuRef, const int N) {
+  double epsilon = 1.0E-8;
+  bool match = 1;
+
+  for (int i = 0; i < N; i++) {
+    if (abs(hostRef[i] - gpuRef[i]) > epsilon) {
+      match = 0;
+      printf("Arrays do not match!\n");
+      printf("host %5.2f gpu %5.2f at current %d\n", hostRef[i], gpuRef[i], i);
+      break;
+    }
+  }
+
+  if (match) printf("Arrays match.\n\n");
 
   return;
 }
@@ -58,8 +78,10 @@ int main(int argc, char **argv) {
   std::cout << "Elapsed time: " << elapsed_gpu.count() * 1000 << " ms\n";
 
   printVector(h_C, nElem);
-  printf("\n");
+  printf("------------------\n");
   printVector(h_C_gpu, nElem);
+  printf("------------------\n");
+  checkResult(h_C, h_C_gpu, nElem);
 
   free(h_A);
   free(h_B);
